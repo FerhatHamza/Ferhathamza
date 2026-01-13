f1fd1e480c401f7705cbd3d2ab0d226747ff8ee0
Ferhat Hamza <Ferhathamza17@gmail.com>
2026-01-14 00:55:44 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,9 @@
-
+-- schema.sql
+DROP TABLE IF EXISTS services;
+CREATE TABLE services (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  description TEXT,
+  link TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
